7ce2d877a54561baf767405b68567d786eb5d71d
Piabonnie <elliot.villafana.pasten.408@gmail.com>
2026-04-20 15:10:10 -0600

    Commit game design document al 90%
    Este commit agrega el GDD ya practicamente finalizado, pero faltando los dibujos para guiar al lector como es visualmente.

---
+++ COMA/Assets/Codigo/LootItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootItem : MonoBehaviour
+{
+[Header("Datos del Loot")]
+public string nombreLoot = "Chatarra";
+public int valor = 10;
+public float peso = 1f;
+public bool destruirAlRecoger = true;
+
+}
